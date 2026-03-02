f1ec15b946de6f25d1be559f988cb6e1d658f1ac
Tiffany Simpson <simpsontiffany84@gmail.com>
2026-03-01 20:57:10 -0500

    Switch to better-sqlite3 for Render compatibility

--- server/config/database.js
+++ server/config/database.js
@@ -2,9 +2,8 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: process.env.NODE_ENV === "production"
-        ? ":memory:"
-        : "./database.sqlite"
+    storage: "./database.sqlite",
+    dialectModule: require("better-sqlite3")
 });
 
 module.exports = sequelize;
