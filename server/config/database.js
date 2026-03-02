e5db8e10dfdce2ef924faf9adef48e7655a0ec6a
Tiffany Simpson <simpsontiffany84@gmail.com>
2026-03-01 21:15:17 -0500

    Switch to PostgreSQL for production deployment

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./database.sqlite",
-    dialectModule: require("better-sqlite3")
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = sequelize;
